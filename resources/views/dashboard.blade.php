51fe1ec6591003c8415384ee47222fdb8d6c6bfb
Akash Tiwari <akash74742211@gmail.com>
2023-11-10 16:01:27 +0530

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+  
+@section('title', 'Dashboard 😀 - Laravel AppLication Useful For Any Business🏢 To Maintain The Data ')
+  
+@section('contents')
+  <div class="row">
+     Dashboard
+  </div>
+@endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+  <div class="container my-auto">
+    <div class="copyright text-center my-auto">
+      <span>Copyright © Made By 🧑‍💻</span>
+    </div>
+  </div>
+</footer>
